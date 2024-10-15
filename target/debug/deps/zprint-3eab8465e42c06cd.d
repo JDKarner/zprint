7a75b94679de7c1c1a87e965bd4fda79092897f9
JDKarner <joshua@karner.dev>
2024-10-15 10:57:36 -0600

    squash?

---
+++ target/debug/deps/zprint-3eab8465e42c06cd.d
@@ -0,0 +1,5 @@
+/home/joshua/zprint/target/debug/deps/libzprint-3eab8465e42c06cd.rmeta: src/main.rs
+
+/home/joshua/zprint/target/debug/deps/zprint-3eab8465e42c06cd.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/zprint-a2ec78473a523bf8.d
@@ -0,0 +1,5 @@
+/home/joshua/zprint/target/debug/deps/libzprint-a2ec78473a523bf8.rmeta: src/main.rs
+
+/home/joshua/zprint/target/debug/deps/zprint-a2ec78473a523bf8.d: src/main.rs
+
+src/main.rs:
